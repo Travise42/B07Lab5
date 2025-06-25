9426cd39a84f273d41e1b8e0b14a26ace3dd05d6
Vansh Anand <130173778+VanshAnand1@users.noreply.github.com>
2025-06-25 11:04:45 -0400

    feat: created Hexagon class
    Co-authored-by: Jannymo13 <jonkonichottil@gmail.com>

---
+++ src/lab5/Hexagon.java
@@ -0,0 +1,27 @@
+package lab5;
+
+public class Hexagon {
+	Point[] vertices;
+	Point centre;
+	double sideLength;
+	
+	public Hexagon(Point centre, Point vertex1) {
+		
+	}
+	
+	public Hexagon(Point centre, double sideLength) {
+		
+	}
+	
+	public double calculateArea() {
+		return 0;
+	}
+	
+	public double calculatePerimeter() {
+		return 0;
+	}
+	
+	
+	
+
+}
