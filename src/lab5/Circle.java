a5ed200ef0efec8240b26a05ca242ee51e3c72e0
Travise <travise42@icloud.com>
2025-06-26 16:01:43 -0400

    docs: Add comments to Circle class

--- src/lab5/Circle.java
+++ src/lab5/Circle.java
@@ -1,5 +1,9 @@
 package lab5;
 
+/**
+ * Circle represents a circle with a given radius and position.
+ * @author Alex Y
+ */
 public class Circle {
     Point center;
     double radius;
@@ -9,19 +13,40 @@ public class Circle {
         this.radius = radius;
     }
     
+    /**
+     * Calculates the circumference (perimeter) of the circle.
+     * @return the circumference of the circle
+     */
     public double circumference() {
+        // A circle's circumference is given by 2πr
         return 2 * Math.PI * radius;
     }
 
+    /**
+     * Calculates the current area of the circle.
+     * @return the area of the circle
+     */
     public double area() {
+        // A circle's area is given by πr²
         return Math.PI * radius * radius;
     }
 
+    /**
+     * Calculates the diameter of the circle.
+     * @return the diameter of the circle
+     */
     public double diameter() {
+        // A circle's diameter is given by 2r
         return 2 * radius;
     }
 
+    /**
+     * Calculates whether this circle is intersecting <code>circle</code>.
+     * @param circle the circle to check for intersection
+     * @return True if intersecting circle, False otherwise
+     */
     public boolean intersectsCircle(Circle circle) {
+        // Two circles intersect each other when their distance is less than or equal to their combined radius
         return center.distance(circle.center) <= this.radius + circle.radius;
     }
 }
